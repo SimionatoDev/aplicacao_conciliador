0dc61ed5ae723e3af0ba09f68db1a8dec2ba0e47
Marcos Renato Falconi <falconi.2050@gmail.com>
2026-04-10 14:35:21 -0300

    Versao 10 04 26 C

---
+++ docs_2026_04_10_14_33/chunk-Y64HCEJG.js
@@ -0,0 +1,1 @@
+import{ra as i}from"./chunk-E4BX5U67.js";var n=class e{transform(r,t=6){return r==null?"":r.toString().padStart(t,"0")}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"zeroFill",type:e,pure:!0})};export{n as a};
